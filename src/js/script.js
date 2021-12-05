bfa7439851a2e7c90b928c73344ec56e57b767f0
João Guedes <junior.guedes@academico.ifrn.edu.br>
2021-12-05 17:01:18 -0300

    Teams concluído

--- src/js/script.js
+++ src/js/script.js
@@ -20,4 +20,26 @@ $(document).ready(() => {
     $('.fa-bars').toggleClass('fa-times')
     $('.fa-times').toggleClass('fa-bars')
   })
+
+  /* owl corousel */
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: false
+      }
+    }
+  })
 })
